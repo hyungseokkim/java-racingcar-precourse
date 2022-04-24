c4ac85d1a5bd0d86693b1c66494d60eac2f6a2c2
김형석 <khkhvv@gmail.com>
2022-04-25 03:26:49 +0900

    feat (domain) : Car에 기능 추가
    1.getPosition()
    2.addWinnerList()
    **지운기능 : getName()

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,7 +1,10 @@
 package racingcar.domain;
 
-import racingcar.view.GameView;
+import racingcar.view.Output;
 import utils.RandomUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Car {
     public static final String PRINT_FLAG_POSITION = "-";
@@ -25,7 +28,14 @@ public class Car {
         for(int i=0; i<this.position; i++){
             stringBuffer.append(PRINT_FLAG_POSITION);
         }
-        GameView.outputMessage(stringBuffer.toString());
+        Output.outputMessage(stringBuffer.toString());
     }
-
+    public int getPosition() {
+        return this.position;
+    }
+    public List<String> addWinnerList(List<String> winnerList){
+        List<String> returnList = new ArrayList<>(winnerList);
+        returnList.add(this.name);
+        return returnList;
+    }
 }
