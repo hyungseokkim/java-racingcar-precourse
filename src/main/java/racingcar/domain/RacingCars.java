d306819fba005c384236fb9d60ddd19ab7f3eced
김형석 <khkhvv@gmail.com>
2022-04-25 03:35:22 +0900

    feat (domain) : CarList 생성용 RacingCars 도메인 추가
    1.makeCar기능
    1-2.makeCarNameList기능
    2.winnerList기능
    2-1.makePositionList기능

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -4,18 +4,19 @@ import utils.ValidationUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 public class RacingCars {
     public static final String REGEX = ",";
+    private final List<Car> cars = new ArrayList<>();
 
     public List<Car> makeCars(String input){
-        List<Car> cars = new ArrayList<>();
         List<String> carNameList = new ArrayList<>(makeCarNameList(input));
         ValidationUtils.validationCarNameList(carNameList);
         for(String each : carNameList){
-            cars.add(new Car(each));
+            this.cars.add(new Car(each));
         }
-        return cars;
+        return this.cars;
     }
     public List<String> makeCarNameList(String input){
         List<String> carNames = new ArrayList<>();
@@ -25,5 +26,22 @@ public class RacingCars {
             carNames.add(trimEach);
         }
         return carNames;
+    }
+    public List<String> winnerList(){
+        List<String> returnList = new ArrayList<>();
+        int maxNumber = Collections.max(this.makePositionList());
+        for(Car car:this.cars){
+            if(maxNumber == car.getPosition()){
+                returnList = car.addWinnerList(returnList);
+            }
+        }
+        return returnList;
+    }
+    private List<Integer> makePositionList(){
+        List<Integer> positions = new ArrayList<>();
+        for(Car car:this.cars){
+            positions.add(car.getPosition());
+        }
+        return positions;
     }
 }
