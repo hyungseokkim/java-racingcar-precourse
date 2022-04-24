69524704ce04e927b5dd353fd0a4e76d19438dc6
김형석 <khkhvv@gmail.com>
2022-04-25 00:22:36 +0900

    feat (domain) : Car 클래스 변경
    테스트코드에서 사용한 getName()메소드 지우고,
    결과 메시지로 출력하도록 outputResult 추가.
    move 메소드 추가.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,19 +1,31 @@
 package racingcar.domain;
 
-import utils.ValidationUtils;
-
-import java.util.ArrayList;
-import java.util.List;
+import racingcar.view.GameView;
+import utils.RandomUtils;
 
 public class Car {
+    public static final String PRINT_FLAG_POSITION = "-";
     private final String name;
-    private final int position = 0;
+    private int position = 0;
+    StringBuffer stringBuffer;
 
     public Car(final String name) {
         this.name = name;
     }
-
-    public String getName() {
-        return this.name;
+    public void move() {
+        int random = RandomUtils.makeRandom();
+        if(random >= 4) {
+            this.position++;
+        }
+        outputResult();
     }
+    public void outputResult(){
+        stringBuffer = new StringBuffer(this.name);
+        stringBuffer.append(" : ");
+        for(int i=0; i<this.position; i++){
+            stringBuffer.append(PRINT_FLAG_POSITION);
+        }
+        GameView.outputMessage(stringBuffer.toString());
+    }
+
 }
